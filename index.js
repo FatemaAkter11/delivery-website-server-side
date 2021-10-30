11c8c935285c7af39859fc61f7864ab4536dd084
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-30 12:44:05 +0600

    get api and post api part added

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pjjkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('connected to database');
+        const database = client.db('hungryNaki');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+
+            // create a document to insert
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+            // res.send('post hitted');
+        });
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to Hungry Naki Server')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log('Hungry Server Port', port);
 })
